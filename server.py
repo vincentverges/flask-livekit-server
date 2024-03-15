011213edfe32eb6a5d0290427bfd0e98b5c40bc1
Vincent Verges <vincentverges@MacBook-Air-de-Vincent.local>
2024-03-15 11:20:17 +0100

    Update server.py

--- server.py
+++ server.py
@@ -4,14 +4,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/getToken')
+@app.route('/token')
 def getToken():
   token = api.AccessToken(os.getenv('LIVEKIT_API_KEY'), os.getenv('LIVEKIT_API_SECRET')) \
-    .with_identity("identity") \
-    .with_name("my name") \
+    .with_identity("raspberry") \
+    .with_name("Raspberry") \
     .with_grants(api.VideoGrants(
         room_join=True,
-        room="my-room",
+        room="rccar",
     ))
   return token.to_jwt()
 
